99e9010b4e61f5ab6a9384eaa34abc30cdde825f
Felipe Barbosa Manhani <fbmanhani@gmail.com>
2019-03-29 21:27:32 -0300

    Initial commit

---
+++ app/src/main/java/br/edu/ifsp/scl/manhani/tradutorsdmkt/Constantes.kt
@@ -0,0 +1,11 @@
+package br.edu.ifsp.scl.manhani.tradutorsdmkt
+
+object Constantes {
+    // Constantes usadas para acesso ao WS de tradução
+    val URL_BASE = "https://od-api.oxforddictionaries.com/api/v1/"
+    val END_POINT = "entries"
+    val APP_ID_FIELD = "app_id"
+    val APP_ID_VALUE = "38eec52b" // Preencher com seu app_id
+    val APP_KEY_FIELD = "app_key"
+    val APP_KEY_VALUE = "d78094be896afae862068760673967be" // Preeencher com seu app_key
+}
---
+++ app/src/main/java/br/edu/ifsp/scl/manhani/tradutorsdmkt/RegionActivity.kt
@@ -0,0 +1,12 @@
+package br.edu.ifsp.scl.manhani.tradutorsdmkt
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class RegionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_region)
+    }
+}
